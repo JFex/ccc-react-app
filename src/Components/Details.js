c612df36ab1aaa8e4609a0a98a6def005d797f6b
Jared Feexico <jfeexico4@gmail.com>
2021-02-09 21:51:57 -0600

    adds a basic layout

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ProductConsumer} from '../context.js';
+import {Link} from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>Details Page</h3>
-            </div>
+            <ProductConsumer>
+                {value => {
+                    const {id, title, img, info, price} = value.detailProducts;
+                    return (
+                        <div className="container py-5">
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center my-5">
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <Carousel>
+                                        <div>
+                                            <img className="d-block w-100" src={require("../images/as-above-main.jpg")} alt="1" />
+                                        </div>
+                                        <div>
+                                            <img className="d-block w-100" src={require("../images/as-above-close-front.jpg")} alt="2" />
+                                        </div>
+                                        <div>
+                                            <img className="d-block w-100" src={require("../images/as-above-distant-front.jpg")} alt="3" />
+                                        </div>
+                                    </Carousel>
+                                </div>
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <h3 className="mb-5">${price}</h3>
+                                    <p className="mb-5">{info}</p>
+                                    <h3 className="mb-5">Buttons go here (S, M, L, XL, XXL)</h3>
+                                    <button className="">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }}
+            </ProductConsumer>
         )
     }
 }
